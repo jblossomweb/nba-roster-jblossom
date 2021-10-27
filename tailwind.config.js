d14b0ffc031c97dddde92cd331a93daada8d9ba9
John Blossom <john@simplecast.com>
2021-10-26 21:56:06 -0600

    add specified breakpoints, with some extras

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    screens: {
+      xs: { max: '399px' },
+      sm: { min: '400px' },
+      md: { min: '640px' },
+      lg: { min: '960px' },
+      xl: { min: '1280px' },
+      xxl: { min: '1580px' }
+    }
   },
   variants: {
     extend: {},
